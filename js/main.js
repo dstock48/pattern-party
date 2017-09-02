1890b7c1e48a8242134df5d71f52d61316906fd6
Dave W <dviper@gmail.com>
2017-09-01 21:26:13 -0600

    update tab switching functionality

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 $('.tab').on('click', (e) => {
-  $(e.target).toggleClass('tab-active');
+  $('.fa').removeClass('fa-minus');
+  $('.fa').addClass('fa-plus');
   const icon = $(e.target).find('.fa');
+  icon.addClass('fa-minus');
 
-  if (icon.hasClass('fa-minus')) {
-    icon.removeClass('fa-minus');
-    icon.addClass('fa-plus');
-  } else {
-    icon.removeClass('fa-plus');
-    icon.addClass('fa-minus');
-  }
+  $('.tab').removeClass('tab-active')
+  $(e.target).toggleClass('tab-active');
+  $('.content').removeClass('content-active')
+  console.log($(e.target).siblings('.content'))
+  $(e.target).siblings('.content').toggleClass('content-active')
+
+  // if (icon.hasClass('fa-minus')) {
+  //   icon.removeClass('fa-minus');
+  //   icon.addClass('fa-plus');
+  // } else {
+  //   icon.removeClass('fa-plus');
+  //   icon.addClass('fa-minus');
+  // }
 });
+
