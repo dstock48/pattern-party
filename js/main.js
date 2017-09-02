deef5f38ef33383929cc1f6c4f4e0bbe73202641
Dave W <dviper@gmail.com>
2017-09-02 13:08:00 -0600

    add desktop layout and functionality

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-$('.tab').on('click', (e) => {
-  $('.fa').removeClass('fa-minus');
-  $('.fa').addClass('fa-plus');
-  $(e.target).find('.fa').addClass('fa-minus');
+$('.accordian-tabs').on('click', '.tab', (e) => {
+  if ([...e.target.classList].includes('tab')) {
+    $('.fa').removeClass('fa-minus');
+    $('.fa').addClass('fa-plus');
+    $(e.target).find('.fa').addClass('fa-minus');
 
-  $('.tab').removeClass('tab-active');
-  $(e.target).addClass('tab-active');
+    $('.tab').removeClass('tab-active');
+    $(e.target).addClass('tab-active');
 
-  $('.content').removeClass('content-active');
-  $(e.target).siblings('.content').toggleClass('content-active');
+    $('.content').removeClass('content-active');
+    $(e.target).siblings('.content').toggleClass('content-active');
+  }
 });
 
