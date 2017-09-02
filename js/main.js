8472a9cc3849d87f2f5d26c95fc5faad90003d22
Dave W <dviper@gmail.com>
2017-09-02 10:46:33 -0600

    organize js file

--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
 $('.tab').on('click', (e) => {
   $('.fa').removeClass('fa-minus');
   $('.fa').addClass('fa-plus');
-  const icon = $(e.target).find('.fa');
-  icon.addClass('fa-minus');
+  $(e.target).find('.fa').addClass('fa-minus');
 
-  $('.tab').removeClass('tab-active')
-  $(e.target).toggleClass('tab-active');
-  $('.content').removeClass('content-active')
-  console.log($(e.target).siblings('.content'))
-  $(e.target).siblings('.content').toggleClass('content-active')
+  $('.tab').removeClass('tab-active');
+  $(e.target).addClass('tab-active');
 
-  // if (icon.hasClass('fa-minus')) {
-  //   icon.removeClass('fa-minus');
-  //   icon.addClass('fa-plus');
-  // } else {
-  //   icon.removeClass('fa-plus');
-  //   icon.addClass('fa-minus');
-  // }
+  $('.content').removeClass('content-active');
+  $(e.target).siblings('.content').toggleClass('content-active');
 });
 
