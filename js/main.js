aa7899e8f4c19f5e1dd62224ebf59c34e8d4631a
Dave W <dviper@gmail.com>
2017-09-03 14:13:15 -0600

    add header bar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+$('a').on('click', () => false);
+
 $('.accordian-tabs').on('click', '.tab', (e) => {
   if ([...e.target.classList].includes('tab')) {
-    $('.fa').removeClass('fa-minus');
-    $('.fa').addClass('fa-plus');
+    $('.accordian-tabs .fa').removeClass('fa-minus');
+    $('.accordian-tabs .fa').addClass('fa-plus');
     $(e.target).find('.fa').addClass('fa-minus');
 
     $('.tab').removeClass('tab-active');
@@ -9,6 +11,21 @@ $('.accordian-tabs').on('click', '.tab', (e) => {
 
     $('.content').removeClass('content-active');
     $(e.target).siblings('.content').toggleClass('content-active');
+
+    if (window.innerWidth < 500) {
+      $('html').scrollTop($(e.target).offset().top - 20);
+    }
   }
 });
 
+$('.menu-btn').on('click', () => {
+  $('.dropdown').toggleClass('hidden');
+});
+
+$('.desktop-header .search-bar input').on('focus', (e) => {
+  $(e.target).addClass('expanded');
+});
+
+$('.desktop-header .search-bar input').on('blur', (e) => {
+  $(e.target).removeClass('expanded');
+});
